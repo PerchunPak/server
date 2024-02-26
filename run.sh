72c3174617a4b0d792d7de9b0887136a502a350b
PerchunPak <git@perchun.it>
2024-02-26 13:23:53 +0100

    Init commit

---
+++ run.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -e
+
+if [ -z "$1" ]; then
+	echo "No argument supplied"
+	exit 1
+fi
+
+function run_file {
+	docker compose up -d -f $1
+}
+
+for FILE in projects/*; do
+	if [[ $1 == "all" ]]; then
+		for FILE in projects/*; do
+			run_file $FILE
+		done
+		break
+	fi
+
+	if [[ $1 != $FILE ]]; then
+		continue
+	fi
+
+	run_file $FILE
+	break
+done
