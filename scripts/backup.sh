a4615aa8a3d0eda7ce536159c1b60684c5eb2122
PerchunPak <git@perchun.it>
2024-03-28 23:19:50 +0100

    Add date instead of random string in backup name

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -16,10 +16,7 @@
 #     gpg --keyserver keys.openpgp.org --recv-keys GPG_KEY_HERE
 set -e
 
-generate_random_string() {
-	echo $(tr -dc A-Za-z0-9 </dev/urandom | head -c 5)
-}
-FILE_NAME="/tmp/data_$(generate_random_string).zip.gpg"
+FILE_NAME="/tmp/data_$(date +"%Y-%m-%d_%H-%M-%S").zip.gpg"
 
 zip -r - $DATA_FOLDER | \
 	gpg --batch --trust-model always -o $FILE_NAME --encrypt -r $GPG_KEY_1 -r $GPG_KEY_2
