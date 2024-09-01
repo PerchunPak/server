f1919fdc6a569a99a3a30af49170f703720cad4f
PerchunPak <git@perchun.it>
2024-09-01 14:43:58 +0200

    Improve backup script

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # What this script does:
 #     - Packs everything in `data` into zip archive.
@@ -14,13 +14,30 @@
 #
 # And do not forget to import recipients' keys:
 #     gpg --keyserver keys.openpgp.org --recv-keys GPG_KEY_HERE
-set -e
+set -ex
 
-FILE_NAME="/tmp/data_$(date +"%Y-%m-%d_%H-%M-%S").zip.gpg"
+FILE_NAME="/tmp/data_$(date +"%Y-%m-%d_%H-%M-%S").tar.gz"
 
-zip -r - $DATA_FOLDER | \
-	gpg --batch --trust-model always -o $FILE_NAME --encrypt -r $GPG_KEY_1 -r $GPG_KEY_2
+rm $FILE_NAME || true
+tar -zcvf "$FILE_NAME" --exclude='*.png' "$DATA_FOLDER"
 
-curl -F document=@\"$FILE_NAME\" "https://api.telegram.org/bot$BOT_TOKEN/sendDocument?chat_id=$CHAT_ID"
+gpg --no-tty --keyserver keys.openpgp.org --recv-keys "$GPG_KEY_1"
+gpg --no-tty --keyserver keys.openpgp.org --recv-keys "$GPG_KEY_2"
+gpg --batch --trust-model always -o "$FILE_NAME.gpg" --encrypt -r "$GPG_KEY_1" -r "$GPG_KEY_2" "$FILE_NAME"
 
-rm $FILE_NAME
+file_size=$(stat --printf="%s" "$FILE_NAME.gpg")
+chunk_size=$((49*1024*1024))  # 49 megabytes
+
+if (( file_size > chunk_size )); then
+    split -b "$chunk_size" "$FILE_NAME.gpg" "$FILE_NAME.gpg.part"
+
+    for part_file in $FILE_NAME.gpg.part*; do
+        curl -F document=@"$part_file" "https://api.telegram.org/bot$BOT_TOKEN/sendDocument?chat_id=$CHAT_ID"
+    done
+
+    rm "$FILE_NAME.gpg.part"*
+else
+    curl -F document=@"$FILE_NAME.gpg" "https://api.telegram.org/bot$BOT_TOKEN/sendDocument?chat_id=$CHAT_ID"
+fi
+
+rm "$FILE_NAME" "$FILE_NAME.gpg" || true
